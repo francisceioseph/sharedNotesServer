5f21b466e62289529a9abdd55c320fe4439a58c3
Francisco José A. C. Souza <francisceioseph@gmail.com>
2015-01-30 11:33:48 -0300

    Declaração dos métodos da interface e descrição da estrutura do json que armazenará os projetos.

--- src/main/java/SharedNotesInterface.java
+++ src/main/java/SharedNotesInterface.java
@@ -1,14 +1,27 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 /**
  * Created by francisco on 30/01/15.
  */
 public interface SharedNotesInterface extends Remote {
 
-    public boolean authenticate(String email, String password) throws RemoteException;
+    //How to generate UUID
+    //String uniqueID = UUID.randomUUID().toString();
+
     public boolean createUser(String username, String email, String password) throws RemoteException;
+    public boolean authenticate(String email, String password) throws RemoteException;
     public void disconnect(String email) throws RemoteException;
 
+    public ArrayList<JSONArray> listAllNotes(String username) throws RemoteException;
+    public boolean createNote(JSONObject note) throws RemoteException;
+    public boolean updateNote(JSONObject note) throws RemoteException;
+    public boolean deleteNote(JSONObject note) throws RemoteException;
+    public boolean deleteNote(int indexNote) throws RemoteException;
+    public JSONObject retrieveNote(int indexNote) throws RemoteException;
 
 }
